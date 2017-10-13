40104d82100aa9714e79cda13c404f6f83105937
Beata <Beata@LENOVO-PC>
2017-10-13 19:39:48 +0200

    changes in main

--- PAG/PAG/main.cpp
+++ PAG/PAG/main.cpp
@@ -60,7 +60,6 @@ int main()
 
 	///////////////////////////////////
 
-	//	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	// build and compile our shader program
 	// ------------------------------------
@@ -162,16 +161,16 @@ int main()
 		// ------
 
 		// T£O
-		//	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		//	glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		// draw our first triangle
 
 		// do shaderow
 		//	glUseProgram(shaderProgram);
 
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+//		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	//	glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 		// glBindVertexArray(0); // no need to unbind it every time 
 
